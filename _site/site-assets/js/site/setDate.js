c835889c84e76ba9f3e803bd862795f9f963e6e1
Robert Crooks <rcrooks@brightcove.com>
2020-10-01 10:03:36 -0400

    more exception handling

--- _site/site-assets/js/site/setDate.js
+++ _site/site-assets/js/site/setDate.js
@@ -2,5 +2,7 @@ var BCLS_set_date = ( function (window, document) {
   var  bc_copyright = document.getElementById('bc_copyright'),
     c = bc_copyright.textContent,
     y = new Date().getFullYear();
-    bc_copyright.innerHTML = '&copy;' + y + '&nbsp;' + c;
+    if (bc_copyright) {
+      bc_copyright.innerHTML = '&copy;' + y + '&nbsp;' + c;
+    }
 })(window, document);
