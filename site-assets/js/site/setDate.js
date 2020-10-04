bf6f939dc0e26c4c165bb414e8edad6604a44563
Robert Crooks <rcrooks@brightcove.com>
2020-10-04 12:38:57 -0400

    fixes

--- site-assets/js/site/setDate.js
+++ site-assets/js/site/setDate.js
@@ -1,8 +1,8 @@
 var BCLS_set_date = ( function (window, document) {
   var  bc_copyright = document.getElementById('bc_copyright'),
-    c = bc_copyright.textContent,
     y = new Date().getFullYear();
-    if (bc_copyright) {
-      bc_copyright.innerHTML = '&copy;' + y + '&nbsp;' + c;
-    }
+  if (bc_copyright) {
+    var c = bc_copyright.textContent;
+    bc_copyright.innerHTML = '&copy;' + y + '&nbsp;' + c;
+  }
 })(window, document);
