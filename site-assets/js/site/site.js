70163c2bc80a1327d4f05739ded9ffdd0cc7edba
Robert Crooks <rcrooks@brightcove.com>
2020-10-04 10:37:57 -0400

    wip embedded docs

--- site-assets/js/site/site.js
+++ site-assets/js/site/site.js
@@ -24,7 +24,8 @@ var BCLS_site = (function(window, document) {
     footer = document.querySelector('footer'),
     talla_wrapper = document.getElementById('talla_wrapper'),
     open_new_tab = document.getElementById('open_new_tab'),
-    bc_veggie_burger_wrapper = document.getElementById('bc_veggie_burger_wrapper');
+    bc_veggie_burger_wrapper = document.getElementById('bc_veggie_burger_wrapper'),
+    in_prod_nav = document.getElementById('in_prod_nav');
 
   // if inside iframe, hide appropriate elements
   if (window.location !== window.parent.location) {
@@ -33,6 +34,7 @@ var BCLS_site = (function(window, document) {
     hideElement(side_nav);
     hideElement(talla_wrapper);
     hideElement(bc_veggie_burger_wrapper);
+    in_prod_nav.removeAttribute('style');
     open_new_tab.removeAttribute('style');
     open_new_tab.setAttribute('href', window.location.href);
     removeFeedbackify();
