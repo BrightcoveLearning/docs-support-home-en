7b0cd79ba3010416acb0f771a03c9badb5b8dd38
Robert Crooks <rcrooks@brightcove.com>
2020-11-18 11:28:25 -0500

    hide burger in iframe

--- site-assets/js/site/toc.js
+++ site-assets/js/site/toc.js
@@ -225,6 +225,10 @@ var BCLS_toc = ( function (window, document) {
           });
         }
 
+         // if inside iframe, hide appropriate elements
+        if (window.location !== window.parent.location) {
+          bc_veggie_burger_wrapper.setAttribute('style', 'display:none');
+
   // this creates a public method, allow it to be run again (imported content for example)
   return {
     create_inpage_nav: create_inpage_nav,
