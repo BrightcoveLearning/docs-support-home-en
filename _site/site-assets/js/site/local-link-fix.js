4d880df922f383a2b7d9c36f1a31dcf9c1c712b6
Robert <rcrooks@brightcove.com>
2021-09-23 11:00:09 -0400

    updating script to localize cross-site links on localized sites

--- _site/site-assets/js/site/local-link-fix.js
+++ _site/site-assets/js/site/local-link-fix.js
@@ -1,5 +1,6 @@
 // keep on same language site
-function keepLanguage() {
+
+var BCLS_keep_language = ( function (window, document) {
   var href = location.href,
     domain = location.hostname,
     lang = domain.split('.')[0],
@@ -9,40 +10,6 @@ function keepLanguage() {
     site_select = document.getElementById('site_select'),
     site_options = site_select.querySelectorAll('option');
 
-    if (href === 'https://support.brightcove.com/en/contact') {
-      location.href = 'https://supportportal.brightcove.com/';
-    }
-    // if old /en/ path, redirect
-    if (href.indexOf('/en/') > 0 ) {
-      if (href.indexOf('/video-cloud/docs/') > 0) {
-        href = href.replace('/video-cloud/docs/', '/');
-      }
-      location.href = href.replace('/en/', '/');
-    }
-
-    // if old localized site path, redirect to new and hope for the best
-    if (href.indexOf('/ja/') > 0) {
-      href = href.replace('/ja/', '/');
-      href = href.replace('//', '//ja.');
-      location.href = href;
-    } else if (href.indexOf('/ko/') > 0) {
-      href = href.replace('/ko/', '/');
-      href = href.replace('//', '//ko.');
-      location.href = href;
-    } else if (href.indexOf('/es/') > 0) {
-      href = href.replace('/es/', '/');
-      href = href.replace('//', '//es.');
-      location.href = href;
-    } else if (href.indexOf('/fr/') > 0) {
-      href = href.replace('/fr/', '/');
-      href = href.replace('//', '//fr.');
-      location.href = href;
-    } else if (href.indexOf('/de/') > 0) {
-      href = href.replace('/de/', '/');
-      href = href.replace('//', '//de.');
-      location.href = href;
-    }
-    
   if (lang === 'ja' || lang === 'ko' || lang === 'es' || lang === 'fr' || lang === 'de' || lang === 'zh-tw') {
     var hrefValue, 
       currentHref = window.location.href,
@@ -54,19 +21,13 @@ function keepLanguage() {
     
     for (i = 0; i < iMax; i++) {
       currentLink = all_links[i];
-      
-      
+      hrefValue = currentLink.split('//');
       currentLinkValue = currentLink.getAttribute('href');
-      
-      hrefValue = currentLinkValue.split('//');
-      
-      if (hrefValue.length === 2) {
-        if (hrefValue[1].indexOf('support.brightcove') > -1) {
+            
+      if (hrefValue.indexOf('support.brightcove.com') > 0) {
           newHref = hrefValue[0] + '//' + lang + '.' + hrefValue[1];
           // console.log('newHref remote', newHref);
           all_links[i].setAttribute('href', newHref);
-          
-        }
       }
     }
       // fix site navigator
@@ -79,7 +40,4 @@ function keepLanguage() {
           
       }
   }
-}
-
-
-keepLanguage();
+})(window, document);
