1e9b517d5cbd27f35ba298ba181f5ef11a9638f1
Robert Crooks <rcrooks@brightcove.com>
2022-02-19 16:00:41 -0500

    ya tweak

--- site-assets/js/site/in-app.js
+++ site-assets/js/site/in-app.js
@@ -77,7 +77,7 @@ function addPlayer() {
     player_data.account_id +
     '" data-player="' +
     player_data.player_id +
-    '" data-embed="default" class="video-js" controls class="vjs-fluid" width="640" height="360"></video-js>';
+    '" data-embed="default" class="video-js" controls class="vjs-fluid" width="350" height="197"></video-js>';
     
   // style outer wrapper
   player_outer_wrapper.setAttribute('style', 'position: relative; display: block; max-width: 100%; max-height: 12em;')
