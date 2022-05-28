076c5a6073ac5b01110354d925adeb57ce7256f3
Robert Crooks <rcrooks@brightcove.com>
2022-05-28 14:51:39 -0400

    rebuilding for nav change

--- install-submodules.sh
+++ install-submodules.sh
@@ -6,10 +6,5 @@ cd ..
 cd assets/images
 git submodule add https://github.com/BrightcoveLearning/s-support-img
 git submodule add https://github.com/BrightcoveLearning/s-feather-img
-cd ..
-git submodule add https://github.com/BrightcoveLearning/s-site-assets-favicons
-git submodule add https://github.com/BrightcoveLearning/s-site-assets-stylesheets
-git submodule add https://github.com/BrightcoveLearning/s-site-assets-scripts
-git submodule add https://github.com/BrightcoveLearning/s-site-assets-images
-cd .. 
+zcd .. 
 git submodule update --init --recursive
